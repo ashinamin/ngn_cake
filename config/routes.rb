97d2c5cba4e0bdf8cddebec1b9bf3bdd26bd6aba
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2024-01-17 08:12:19 +0000

    add admin/customers,view file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
