d9c64f810c81e1d1874cf7b5057764585e310c8b
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-01-17 15:21:37 +0000

    add text_field

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,9 +3,13 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    item = Item.new(item_params)
+    item = save
+    redirect_to '/admin/show'
   end
 
   def show
@@ -15,5 +19,11 @@ class Admin::ItemsController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :image)
   end
 end
