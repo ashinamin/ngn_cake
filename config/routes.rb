a503e60fde42307a8ac2c9ca1720b00cda9a64c3
EC2 Default User <149554348+AoKeito@users.noreply.github.com>
2024-01-18 04:48:47 +0000

    success

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'homes/top' => 'homes#top'
+  
   devise_for :customers
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
