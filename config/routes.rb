600fb1e9bd3ac770065270b662db2f7fc0989ea9
EC2 Default User <149554348+AoKeito@users.noreply.github.com>
2024-01-20 02:05:47 +0000

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
   
-  #root :to =>"homes#top"
+  #root to: =>"homes#top"
   
   namespace :admin do
     get "/" => "homes#top"
