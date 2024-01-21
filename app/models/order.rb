9d17454f488b0f17aa4667f1061a7bbfe69d2cbf
EC2 Default User <149554348+AoKeito@users.noreply.github.com>
2024-01-21 05:52:35 +0000

    orders_new

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  has_many :ordered_items
+  has_many :items, through: :ordered_items
+  
+  enum status: {
+    
+  }
+  enum payment_method: { credit_card: 0, transfer: 1 }
 end
