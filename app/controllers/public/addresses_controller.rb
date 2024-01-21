762ca7b24e49063465bcca5436c8e9f79c58bd80
EC2 Default User <149554348+AoKeito@users.noreply.github.com>
2024-01-21 15:45:37 +0000

    address

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,16 +1,32 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.new
+    @addresses = Address.all
   end
 
   def edit
+    @address = Address.find(params[:id])
   end
 
   def create
+    @address = Address.new(address_params)
+    @address.customer_id = current_customer.id
+    @address.save
+    redirect_to public_addresses_path
   end
 
   def update
+    address = Address.find(params[:id])
+    address.update(address_params)
+    redirect_to public_addresses_path
   end
 
   def destroy
   end
+  
+  private
+  def address_params
+    params.require(:address).permit(:postal_code, :address, :name)
+  end
+  
 end
