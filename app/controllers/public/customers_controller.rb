87f5946ef06127d3826bd3addc4b69473b795eb6
ystm08 <150111719+ystm08@users.noreply.github.com>
2024-01-22 15:14:59 +0000

    Finish　ルーティング変更後の最終編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -13,7 +13,7 @@ class Public::CustomersController < ApplicationController
     @customer = current_customer
     if @customer.update(customer_params)
      flash[:success] = "登録情報を変更しました。"
-     redirect_to public_customers_my_page_path
+     redirect_to customers_my_page_path
     else
      render 'edit'
     end
