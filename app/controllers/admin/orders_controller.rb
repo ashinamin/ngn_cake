a03a2897bc7ce673b660d8562744adf62573de82
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2024-01-24 03:39:09 +0000

    コントローラーテスト前完成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -5,6 +5,23 @@ class Admin::OrdersController < ApplicationController
   end
 
   def update
-    
+    @order= Order.find(params[:id])
+    @order.update(order_params)
+    @order_details= @order.order_details
+
+    if @order.status == "payment_confirmation"
+      @order_details.each do |order_detail|
+        order_detail.making_status = "waiting_for_payment"
+        order_detail.save
+      end
+    end
+    redirect_to admin_order_path
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:status)
+  end
+
 end
