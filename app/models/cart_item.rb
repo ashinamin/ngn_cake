e9c56de2e25ab5d09550acfe5946b4bdac8e8a87
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-01-24 03:03:00 +0000

    test コピー

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,12 +1,16 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :item
-  
+
   def add_tax_price
     (self.price * 1.10).round
   end
-  
+
   def subtotal
     item.add_tax_price * amount
-  end  
+  end
+
+  def update_total
+    update(total: cart_items.sum(&:subtotal))
+  end
 end
