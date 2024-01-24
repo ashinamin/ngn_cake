fe93b89d695d6b5114a2b1ca9d3a88e0eac01760
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-01-24 02:52:48 +0000

    test コピー

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,13 @@
 class Public::ItemsController < ApplicationController
+
   def index
+    @genres = Genre.all
     @items = Item.page(params[:page]).per(8)
   end
 
   def show
+    @genres = Genre.all
     @item = Item.find(params[:id])
-    @cart_item = Cart_item.new
+    @cart_item = CartItem.new
   end
 end
