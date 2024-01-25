38e568948436f1a1ee644f7f5425dc04a16922e5
EC2 Default User <149554348+AoKeito@users.noreply.github.com>
2024-01-25 07:03:58 +0000

    confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -55,7 +55,6 @@ class Public::OrdersController < ApplicationController
 
   def show
     @order = current_customer.orders.find(params[:id])
-    #@order_details = @order.order_details
   end
   
   def order_params
