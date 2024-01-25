21b65c5dd104e06603720f735d335f16eb6c4c9e
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2024-01-25 00:45:31 +0000

    render表示

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
     @new_items= Item.order(created_at: :desc).limit(4)
-    @item= Item.find(params[:id])
     @genres= Genre.all
   end
 
