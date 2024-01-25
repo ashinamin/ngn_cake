174542899ee4c8580ea08437bca00f2afde1d60f
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2024-01-25 07:57:50 +0000

    製作ステータスと注文ステータスを同期

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,9 +3,9 @@ class Order < ApplicationRecord
   has_many :cart_items
   has_many :items, through: :cart_items
   has_many :order_details, dependent: :destroy
-  enum status: {入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
+  
+  enum status: {waiting_for_payment: 0, payment_confirmation: 1, making: 2, shipping_in_process: 3, shipping_completed: 4}
   enum payment_method: { credit_card: 0, transfer: 1 }
-  has_many :items
 
   def self.order_statuses_i18n
         order_statuses.keys.map do |status|
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,5 +2,5 @@ class OrderDetail < ApplicationRecord
     belongs_to :order
     belongs_to :item
     
-    enum making_status: {着手不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3}
+    enum making_status: {unable_start: 0, waiting_for_production: 1, in_making: 2, making_completed: 3}
 end
