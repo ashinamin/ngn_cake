788da504029e25bfa05a7e4f5f1a62290f6d7633
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-01-25 04:44:28 +0000

    テスト後修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  
  devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
@@ -24,9 +24,11 @@ Rails.application.routes.draw do
 
   scope module: :public do
     get "/" => "homes#top"
-    get "customers/unsubscribe" => "customers#unsubscribe"
-    get "cart_items/destroy_all" => "cart_items#destroy_all"
-    get "orders/confirm" => "orders#confirm"
+    get "about" => "homes#about"
+    get "customers/unsubscribe" => "customers#unsbscribe"
+    get "customers/withdraw" => "customers#withdraw"
+    delete "cart_items/destroy_all" => "cart_items#destroy_all"
+    #get "orders/confirm" => "orders#confirm"
     get "orders/thanks" => "orders#thanks"
     get "customers/my_page" => "customers#show"
     get "customers/information/edit" => "customers#edit"
@@ -34,11 +36,11 @@ Rails.application.routes.draw do
     #resources :registrations, only: [:new, :create]
     #resources :sessions, only: [:new, :create, :destroy]
     resources :items, only: [:index, :show]
-    # resources :customers, only: [:show, :edit, :update]
-    resources :cart_items, only: [:index, :update, :destroy]
+    #resources :customers, only: [:show, :edit, :update]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
     resources :orders, only: [:new, :index, :show, :create]
+      post "orders/confirm" => "orders#confirm"
     resources :addresses, only: [:index, :edit, :create, :update, :destroy]
-
     resources :customers do
      collection do
        get 'confirm_withdraw'
