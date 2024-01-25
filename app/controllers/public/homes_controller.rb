ff9b6765e953337bae7b84b0fc28d8d1ad830b33
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2024-01-25 02:39:11 +0000

    会員編集画面

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,7 @@ class Public::HomesController < ApplicationController
   def top
     @genres = Genre.all
     @new_items= Item.order(created_at: :desc).limit(4)
-    @items= Item.all
+    
     @genres= Genre.all
   end
 
