b5c88638e8acded938ccdd1906a5cfc88e95772f
suzuki414 <152411546+suzuki414@users.noreply.github.com>
2024-01-25 17:05:18 +0000

    test

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,11 +1,6 @@
 class OrderDetail < ApplicationRecord
     belongs_to :order
-<<<<<<< HEAD
-    belongs_to :customer
-
-=======
     belongs_to :item
-    
->>>>>>> f7f9fe52bd726a61ea1d27f1eb76a5d92e99933b
+    belongs_to :customer
     enum making_status: {着手不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3}
 end
