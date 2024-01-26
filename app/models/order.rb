24e4a7b2efa74e3780da33bd42e309f263e24ee0
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2024-01-26 02:34:44 +0000

    編集済み

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,15 +1,15 @@
 class Order < ApplicationRecord
-<<<<<<< HEAD
-
-
-
-=======
   belongs_to :customer
->>>>>>> f7f9fe52bd726a61ea1d27f1eb76a5d92e99933b
-  has_many :ordered_items
-  has_many :items, through: :ordered_items
+  has_many :cart_items
+  has_many :items, through: :cart_items
   has_many :order_details, dependent: :destroy
-  enum status: {入金待ち: 0, 入金確認: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4}
+
+  enum status: {waiting_for_payment: 0, payment_confirmation: 1, making: 2, shipping_in_process: 3, shipping_completed: 4}
   enum payment_method: { credit_card: 0, transfer: 1 }
 
+  def self.order_statuses_i18n
+        order_statuses.keys.map do |status|
+          [I18.t("enums.order.status.#{status}"), status]
+        end.to_h
+  end
 end
